db86a39681ecbb3210020b60cbaca7d931ecfd3e
Jessica Roussy <jroussy.dev@gmail.com>
2026-04-21 09:24:15 +0200

    Create page Home

--- sunbanks-tool/src/App.tsx
+++ sunbanks-tool/src/App.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Home from "./pages/Home"
+
 
 function App() {
 
@@ -10,7 +7,9 @@ function App() {
     <div>
       <h1>Sunbanks Tool</h1>
     </div>
+    <Home />
   )
 }
+
 
 export default App
